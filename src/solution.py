f63a5c6d73abc4b83c7a72ab90065775a6a2c4e6
Sruthy Venu <129507277+svenu96@users.noreply.github.com>
2024-10-04 11:59:48 +0200

    TODO updated

--- src/solution.py
+++ src/solution.py
@@ -5,7 +5,12 @@ import config as cfg
 def solution_task2():
     """
     Solves the system of linear equations to find the velocity profile inside a pipe.
-
+    
+    TODO: Implement the function to solve the system of linear equations to find the velocity profile inside a pipe. 
+    Write a Python program to solve the system of linear equations obtained from difference equations to solve for u_z
+    as a function of r, and plot the u_z as a function of r to visualize the velocity profile in the pipe for the 
+    given Boundary condition and Parameters.
+    
     """
 
 
@@ -13,13 +18,19 @@ def solution_task3():
     """
     Solves the system of linear equations to find the velocity profile inside a pipe.
 
+    TODO: Implement the function to solve the system of linear equations to find the velocity profile inside a pipe. 
+    Write a Python program to model flow in pipe, where viscosity of the fluid is temperature dependent given by 
+    Eq 17, and the temperature varies with radius as given by Eq 18 (refer tasks list).
+
     """
 
 
 def solution_task4():
     """
 
-    Solves the velocity profile for an incompressible Newtonian fluid flow inside 
-    a pipe using finite differences. 
+    Solves the velocity profile for an incompressible Newtonian fluid flow inside a pipe using finite differences. 
+    
+    TODO: Write Python program to solve the system of linear equations obtained by converting the Eq 19 (refer tasks list)
+    into difference equation. Create a a 3D plot of velocity profile as a function of both radial (r) and axial (z) coordinates.
 
     """
